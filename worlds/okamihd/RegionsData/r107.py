2d9b89c6d9eb7c171a5742716019378d81f0aaeb
Ragmoa <ryckaert.guillaume@neuf.fr>
2025-08-22 18:21:25 +0200

    fix moon cave access logic later

--- worlds/okamihd/RegionsData/r107.py
+++ worlds/okamihd/RegionsData/r107.py
@@ -46,6 +46,7 @@ events = {
     RegionNames.GALE_SHRINE_BOSS: {
         # Techinally galestrom is already required to beat the boss,
         # but if we ever randomize enemies/bosses, I've added the following Susano cutscene requirements here.
+        # FIXME: Add a aquire serpent crystal event to complete this one. (see fixme below)
         "Gale Shrine - Defeat Crimson Helm": EventData(mandatory_enemies=[OkamiEnnemies.CRIMSON_HELM],
                                                        power_slash_level=1,
                                                        required_brush_techniques=[BrushTechniques.GALESTROM],
@@ -94,6 +95,8 @@ locations = {
                                                                            BrushTechniques.GALESTROM])
     },
     RegionNames.GALE_SHRINE_BOSS:{
+        #FIXME: This event is never given since we changed its name to serpent crystal for Moon Cave access.
+
         "Gale Shrine - Crimson Helm Reward": LocData(170, required_items_events=["Gale Shrine - Defeat Crimson Helm"])
     }
 }
